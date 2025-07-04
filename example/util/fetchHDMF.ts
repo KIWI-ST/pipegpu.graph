f57cc0c83a1457a60cdb9174621a4c1f2b8c8286
axmand <815966508@qq.com>
2025-07-04 17:38:31 +0800

    revise

--- example/util/fetchHDMF.ts
+++ example/util/fetchHDMF.ts
@@ -1,12 +1,45 @@
+/**
+ * 
+ */
+interface ILodBound {
+    centerX: number,
+    centerY: number,
+    centerZ: number,
+    radius: number,
+    error: number
+}
 
+/**
+ * 
+ */
+interface IMeshlet {
+    self: ILodBound,
+    parent: ILodBound,
+    indices: Uint32Array
+}
 
+/**
+ * 
+ */
+interface IMesh {
+    vertices: Float32Array;
+    meshlets: IMeshlet[]
+}
 
-const fetchHDMF = async (uri: string, key: string = ""): Promise<void> => {
+/**
+ * 
+ * @param uri 
+ * @param key 
+ */
+const fetchHDMF = async (uri: string, key: string = ""): Promise<IMesh> => {
     const response = await fetch(uri);
     if (!response.ok) {
         throw new Error(`[E][fetchHDMF ] .hdmf load failed, response code: ${response.status}`);
     }
     const json = await response.json();
+    if (json.buffers.length != 1) {
+        throw new Error(`[E][fetchHDMF] invalid .hdmf format, please check.`);
+    }
     const str: string = json.buffers[0].uri;
     const base64Str = str.replace('data:application/octet-stream;base64,', '');
     const binaryString = atob(base64Str);
@@ -15,10 +48,16 @@ const fetchHDMF = async (uri: string, key: string = ""): Promise<void> => {
     for (let i = 0; i < length; i++) {
         uint8Array[i] = binaryString.charCodeAt(i);
     }
-
     const meshletBoundLength = 5 * 4 * 2;
 
-    const vertices: Float32Array = new Float32Array(uint8Array.buffer, 0, 2016 / 4);
+    // bufferview [0] must vertices
+    const vertices: Float32Array = new Float32Array(uint8Array.buffer, 0, json.bufferViews[0].byteLength / 4);
+
+
+
+
+
+    // const vertices: Float32Array = new Float32Array(uint8Array.buffer, 0, 2016 / 4);
     const meshletBounds: Float32Array = new Float32Array(uint8Array.buffer, 2016, meshletBoundLength / 4);
     const meshletIndex: Uint32Array = new Uint32Array(uint8Array.buffer, 2016 + meshletBoundLength, (808 - meshletBoundLength) / 4);
 
