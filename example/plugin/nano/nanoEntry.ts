87c10fe0a81f6378bc008efe600e0bc5046bd3fd
axmand <815966508@qq.com>
2025-07-17 17:31:58 +0800

    revise

--- example/plugin/nano/nanoEntry.ts
+++ example/plugin/nano/nanoEntry.ts
@@ -5,7 +5,8 @@ import {
     ColorAttachment,
     Context,
     Attributes,
-    Uniforms
+    Uniforms,
+    IndexedBuffer
 } from 'pipegpu';
 
 import * as Cesium from 'cesium'
@@ -35,8 +36,9 @@ import { PSEUDOMERCATOR, WGS84 } from './earth/Ellipsoid';
 import { fetchJSON, type Instance, type InstanceDataPack } from '../../util/fetchJSON';
 import { fetchKTX2AsBc7RGBA, type KTXPackData } from '../../util/fetchKTX';
 import { DebugMeshletComponent } from '../../../shaderGraph/component/DebugMeshletComponent';
-import type { Mat4, Vec4 } from 'pipegpu.matrix';
+import { GLMatrix, type Mat4, type Vec4 } from 'pipegpu.matrix';
 import { GeodeticCoordinate } from './earth/GeodeticCoordinate';
+import { IndexedStorageBuffer } from 'pipegpu/src/res/buffer/IndexedStorageBuffer';
 
 
 type InstanceDesc = {
@@ -53,6 +55,85 @@ type MeshDesc = {
 };
 
 const nanoEntry = async (SCENE_CAMERA: Cesium.Camera) => {
+
+    {
+        const spacePosition = Cesium.Cartesian3.fromDegrees(116.3955392, 39.916, 0);
+        const serverModelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(spacePosition);
+
+        const projection_matrix = Cesium.Matrix4.fromArray([
+            1.7320508075688774,
+            0,
+            0,
+            0,
+            0,
+            1.7320508075688774,
+            0,
+            0,
+            0,
+            0,
+            -1.00000000002,
+            -1,
+            0,
+            0,
+            -0.200000000002,
+            0
+        ]);
+
+        const inverse_view_matrix = Cesium.Matrix4.fromArray([
+            -0.8957463748843745,
+            -0.44456544161855593,
+            -3.33066907387547e-16,
+            0,
+            0.28526160358279085,
+            -0.5747681294629972,
+            0.7669859287333648,
+            0,
+            -0.3409754381225668,
+            0.6870248652502368,
+            0.6416639191391532,
+            0,
+            -2177859.8296980904,
+            4388127.966843162,
+            4070964.737855963,
+            1
+        ]);
+
+        const view_matrix = Cesium.Matrix4.fromArray([
+            -0.8957463748843745,
+            0.28526160358279085,
+            -0.3409754381225668,
+            0,
+            -0.44456544161855593,
+            -0.5747681294629972,
+            0.6870248652502368,
+            0,
+            -3.33066907387547e-16,
+            0.7669859287333648,
+            0.6416639191391532,
+            0,
+            1.5887342789753114e-9,
+            21043.220439694356,
+            -6369540.923092016,
+            1
+        ]);
+
+        const vertex_position = Cesium.Cartesian4.fromArray([
+            -4.571963310241699,
+            4.105109691619873,
+            10.755314826965332,
+            1.0
+        ]);
+
+        let mvp_matrix = new Cesium.Matrix4();
+        Cesium.Matrix4.multiply(projection_matrix, view_matrix, mvp_matrix);
+        Cesium.Matrix4.multiply(mvp_matrix, serverModelMatrix, mvp_matrix);
+
+        let ndc_Cartesian4 = new Cesium.Cartesian4();
+        Cesium.Matrix4.multiplyByVector(mvp_matrix, vertex_position, ndc_Cartesian4);
+        console.warn(`ndc x: ${ndc_Cartesian4.x / ndc_Cartesian4.w}`);
+        console.warn(`ndc y: ${ndc_Cartesian4.y / ndc_Cartesian4.w}`);
+        console.warn(`ndc z: ${ndc_Cartesian4.z / ndc_Cartesian4.w}`);
+    }
 
     // const r = WGS84.geographicToSpace(new GeodeticCoordinate(114.2978538661029179491913330511, 30.77193874122344863588183053804, 0));
     // console.log(` ---- ${r} ---`);
@@ -366,6 +447,13 @@ const nanoEntry = async (SCENE_CAMERA: Cesium.Camera) => {
 
     const WGSLCode = debugMeshletComponent.build();
 
+
+
+
+
+
+
+
     let desc: RenderHolderDesc = {
         label: '[DEMO][render]',
         vertexShader: compiler.createVertexShader({
@@ -378,7 +466,7 @@ const nanoEntry = async (SCENE_CAMERA: Cesium.Camera) => {
         }),
         attributes: new Attributes(),
         uniforms: new Uniforms(),
-        dispatch: new RenderProperty(6, 1),
+        // dispatch: new RenderProperty(6, 1),
         colorAttachments: colorAttachments,
         depthStencilAttachment: depthStencilAttachment,
     };
@@ -407,22 +495,22 @@ const nanoEntry = async (SCENE_CAMERA: Cesium.Camera) => {
                 // view matrix
                 // inverseViewMatrix
                 // viewMatrix
-                SCENE_CAMERA.inverseViewMatrix[0],
-                SCENE_CAMERA.inverseViewMatrix[1],
-                SCENE_CAMERA.inverseViewMatrix[2],
-                SCENE_CAMERA.inverseViewMatrix[3],
-                SCENE_CAMERA.inverseViewMatrix[4],
-                SCENE_CAMERA.inverseViewMatrix[5],
-                SCENE_CAMERA.inverseViewMatrix[6],
-                SCENE_CAMERA.inverseViewMatrix[7],
-                SCENE_CAMERA.inverseViewMatrix[8],
-                SCENE_CAMERA.inverseViewMatrix[9],
-                SCENE_CAMERA.inverseViewMatrix[10],
-                SCENE_CAMERA.inverseViewMatrix[11],
-                SCENE_CAMERA.inverseViewMatrix[12],
-                SCENE_CAMERA.inverseViewMatrix[13],
-                SCENE_CAMERA.inverseViewMatrix[14],
-                SCENE_CAMERA.inverseViewMatrix[15],
+                SCENE_CAMERA.viewMatrix[0],
+                SCENE_CAMERA.viewMatrix[1],
+                SCENE_CAMERA.viewMatrix[2],
+                SCENE_CAMERA.viewMatrix[3],
+                SCENE_CAMERA.viewMatrix[4],
+                SCENE_CAMERA.viewMatrix[5],
+                SCENE_CAMERA.viewMatrix[6],
+                SCENE_CAMERA.viewMatrix[7],
+                SCENE_CAMERA.viewMatrix[8],
+                SCENE_CAMERA.viewMatrix[9],
+                SCENE_CAMERA.viewMatrix[10],
+                SCENE_CAMERA.viewMatrix[11],
+                SCENE_CAMERA.viewMatrix[12],
+                SCENE_CAMERA.viewMatrix[13],
+                SCENE_CAMERA.viewMatrix[14],
+                SCENE_CAMERA.viewMatrix[15],
             ]);
             return {
                 rewrite: true,
@@ -505,32 +593,41 @@ const nanoEntry = async (SCENE_CAMERA: Cesium.Camera) => {
 
     // instance desc buffer
     {
-        let instanceBufferOffset = 0;
-        const instanceBufferHandler: Handle2D = () => {
+        let instanceDescBufferOffset = 0;
+        const instanceDescBufferHandler: Handle2D = () => {
             if (instanceDescArray.length) {
                 const details: any = [];
                 let instanceDesc: InstanceDesc | undefined = instanceDescArray.shift();
+                // serverModelMatrix
                 while (instanceDesc) {
+
+                    let instanceMatrix = new Cesium.Matrix4(
+                        instanceDesc.model.value[0], instanceDesc.model.value[1], instanceDesc.model.value[2], instanceDesc.model.value[3],
+                        instanceDesc.model.value[4], instanceDesc.model.value[5], instanceDesc.model.value[6], instanceDesc.model.value[7],
+                        instanceDesc.model.value[8], instanceDesc.model.value[9], instanceDesc.model.value[10], instanceDesc.model.value[11],
+                        instanceDesc.model.value[12], instanceDesc.model.value[13], instanceDesc.model.value[14], instanceDesc.model.value[15]
+                    );
+                    let modelMatrix = new Cesium.Matrix4();
+                    // Cesium.Matrix4.multiply(serverModelMatrix, instanceMatrix, modelMatrix);
+                    modelMatrix = serverModelMatrix;
                     const buffer = new ArrayBuffer(80);
                     const f32view = new Float32Array(buffer, 0, 16);
                     const u32View = new Float32Array(buffer, f32view.byteLength, 1);
                     f32view.set([
-                        instanceDesc.model.value[0], instanceDesc.model.value[1], instanceDesc.model.value[2], instanceDesc.model.value[3],
-                        instanceDesc.model.value[4], instanceDesc.model.value[5], instanceDesc.model.value[6], instanceDesc.model.value[7],
-                        instanceDesc.model.value[8], instanceDesc.model.value[9], instanceDesc.model.value[10], instanceDesc.model.value[11],
-                        instanceDesc.model.value[12], instanceDesc.model.value[13], instanceDesc.model.value[14], instanceDesc.model.value[15],
+                        modelMatrix[0], modelMatrix[1], modelMatrix[2], modelMatrix[3],
+                        modelMatrix[4], modelMatrix[5], modelMatrix[6], modelMatrix[7],
+                        modelMatrix[8], modelMatrix[9], modelMatrix[10], modelMatrix[11],
+                        modelMatrix[12], modelMatrix[13], modelMatrix[14], modelMatrix[15],
                     ]);
                     u32View.set([
                         instanceDesc.mesh_id
                     ]);
-
-
                     details.push({
                         byteLength: 80, // instance align byte length
-                        offset: instanceBufferOffset,
+                        offset: instanceDescBufferOffset,
                         rawData: buffer,
                     });
-                    instanceBufferOffset += buffer.byteLength;
+                    instanceDescBufferOffset += buffer.byteLength;
                     instanceDesc = instanceDescArray.shift();
                 }
                 return {
@@ -546,8 +643,24 @@ const nanoEntry = async (SCENE_CAMERA: Cesium.Camera) => {
             }
         }
         // support 10,0000 entites rendering.
-        const instanceDescBuffer = instanceDescSnippet.getBuffer(instanceBufferHandler, 100000 * 80);
+        const instanceDescBuffer = instanceDescSnippet.getBuffer(instanceDescBufferHandler, 100000 * 80);
         desc.uniforms?.assign(instanceDescSnippet.getVariableName(), instanceDescBuffer);
+    }
+
+    // ref:https://github.com/KIWI-ST/pipegpu/blob/main/example/tech/initMultiDrawIndexedIndirect.ts
+    // indirect draw count buffer
+    {
+
+    }
+
+    // indirect command buffer
+    {
+
+    }
+
+    // indirect indexstorage buffer
+    {
+
     }
 
     // raf
